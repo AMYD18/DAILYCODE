c9ca234bcb7b71b5f4a48441e8c18eb17880058b
AMAN <167700482+AMYD18@users.noreply.github.com>
2024-09-27 00:36:36 +0530

    Add files via upload

---
+++ day1Q1.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+	int Num, reverseNum = 0, originalNum, remainder;
+	
+	// reading a five digit number
+	printf("Enter a Five digit number: ");
+	scanf("%d", &Num);
+	
+	origialNum= Num;
+	
+	//Reverse the number
+	while(Num!=0)
+	{remainder=Num%10;
+	reverseNum=reverseNum*10+remainder;
+	Num /=10;
+	}
+	//output of reverse number.
+	
+	printf("reversed number is : %d\n",reverseNum);
+	
+	return 0;
+	
+}
